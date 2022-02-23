91299d1477b1dd377ecce799ec2b0518178ebf24
nikit696 <vqwerty2099@gmail.com>
2022-02-23 22:55:11 +0300

    formPrompt

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstName = prompt('What is your name?')
+let lastName = prompt('What is your Lastname?')
+let age = prompt ('What is you years old?')
+console.log(`Hello, ${firstName} ${lastName}. You are a grown, because you are ${age} years old`)
